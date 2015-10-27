98576446c23ff285970b552a56e2ded1644a1eb9
Dmitry Orlov <me@mosquito.su>
2015-10-28 01:12:33 +0300

    [fix] init sqlalchemy support

--- mytor/sqlalchemy.py
+++ mytor/sqlalchemy.py
@@ -9,17 +9,21 @@ class Engine(BaseEngine):
     pass
 
 
-class TornadoStrategy(DefaultEngineStrategy):
-    name = "tornado"
-    engine_cls = Engine
-
-strategy = TornadoStrategy()
-
-
 class Dialect(MySQLDialect_pymysql):
     pass
 
 
+class TornadoStrategy(DefaultEngineStrategy):
+    name = "tornado"
+    engine_cls = Engine
+
+_STRATEGY = TornadoStrategy()
+
+
 def create_engine(*args, **kwargs):
-    registry.register('mysql.mytor', 'mytor.sqlalchemy', 'Dialect')
-    return strategy.create(*args, **kwargs)
+    module_name = 'mysql.mytor'
+
+    if module_name not in registry.impls:
+        registry.register(module_name, 'mytor.sqlalchemy', 'Dialect')
+
+    return _STRATEGY.create(*args, **kwargs)
